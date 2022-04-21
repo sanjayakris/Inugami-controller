4415004783a8da99725414dd17827452c0152489
Kris <77112262+sanjayakris@users.noreply.github.com>
2022-04-21 13:51:17 +0700

    Fixing stuff and cleaning code

--- inugamiConsolee/Program.cs
+++ inugamiConsolee/Program.cs
@@ -1,39 +1,37 @@
-﻿using System.Text;
-using System.Text.Json;
-using Inugami.Client;
-using Inugami.API;
+﻿using Inugami.API;
 
 namespace Inugami
 {
     class Program
     {
+
         static public async void command(string cmd)
         {
             if (cmd.StartsWith("play"))
             {
                 string[] acts = cmd.Split('_');
-                await postRequest("PLAYING", acts[1]);
+                await APICall.postRequest("PLAYING", acts[1]);
             } else if (cmd.StartsWith("stream")) 
             {
                 string[] acts = cmd.Split('_');
-                await postRequest("STREAMING", acts[1]);
+                await APICall.postRequest("STREAMING", acts[1]);
 
             } else if (cmd.StartsWith("watch")) 
             {
                 string[] acts = cmd.Split('_');
-                await postRequest("WATCHING", acts[1]);
+                await APICall.postRequest("WATCHING", acts[1]);
 
             } else if (cmd.StartsWith("listen"))
             {
                 string[] acts = cmd.Split('_');
-                await postRequest("LISTENING", acts[1]);
+                await APICall.postRequest("LISTENING", acts[1]);
 
             }
 
             if (cmd.StartsWith("nick"))
             {
                 string[] names = cmd.Split('_');
-                await changeNickName(names[1]);
+                await APICall.changeNickName(names[1]);
             }
         }
         static public async Task Main()
